8cb09acac720876c3dbad521a0a96850d068fabf
Daniel Kurz <danielkurz03@protonmail.com>
2023-08-06 11:37:24 +0200

    put compilation bins in folder

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-deno task compile --target x86_64-unknown-linux-gnu --output remarkable-linux-x64
-deno task compile --target x86_64-pc-windows-msvc --output remarkable-windows-x64
-deno task compile --target x86_64-apple-darwin --output remarkable-apple-x64
-deno task compile --target aarch64-apple-darwin --output remarkable-apple-arm
+deno task compile --target x86_64-unknown-linux-gnu --output bin/remarkable-linux-x64
+deno task compile --target x86_64-pc-windows-msvc --output bin/remarkable-windows-x64
+deno task compile --target x86_64-apple-darwin --output bin/remarkable-apple-x64
+deno task compile --target aarch64-apple-darwin --output bin/remarkable-apple-arm
